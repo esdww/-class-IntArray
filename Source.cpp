469d34f68887148730278dc877463e0980246e93
esdww <110739936+esdww@users.noreply.github.com>
2022-08-19 16:22:21 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include "IntArray.h"
+#include"IntArray.cpp"
+#include <exception>
+
+
+
+int main()
+{
+
+        IntArray array(12);
+        
+        for (int i{ 0 }; i < 10; ++i)
+            array[i] = i + 1;
+
+        try
+        {
+            array.resize(8);
+        }
+
+        catch (exception& e)
+        {
+            cout << e.what() << endl;
+        }
+       
+        
+        try
+        {
+            array.insertBefore(20, 3);
+        }
+
+        catch (exception& e)
+        {
+            cout << e.what() << endl;
+        }
+     
+        
+        try
+        {
+            array.remove(3);
+        }
+
+        catch (exception& e)
+        {
+            cout << e.what() << endl;
+        }
+        
+        array.insertAtEnd(30);
+        array.insertAtBeginning(40);
+
+       
+        for (int i{ 0 }; i < array.getLength(); ++i)
+            std::cout << array[i] << ' ';
+
+        std::cout << '\n';
+
+        return 0;
+    
+   
+   
+}
